74896602372f243705306dc99f696c9f71e95a30
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-04-13 08:57:50 -0400

    feat: Add GameObjectComponent plugins

---
+++ CorundumGames.Codegen.Redux/GameObjectComponent/GameObjectComponentConfig.cs
@@ -0,0 +1,32 @@
+﻿using Genesis.Shared;
+
+namespace CorundumGames.Codegen.Redux.GameObjectComponent;
+
+/// <summary>
+/// A configuration definition for the line-endings code-generation output should have.
+/// </summary>
+internal sealed class GameObjectComponentConfig : AbstractConfigurableConfig
+{
+    public const string ComponentContextsKey = "CorundumGames.Codegen.Redux.GameObjectComponent.Contexts";
+    private const string ComponentContextsDefault = "Game, Input";
+    public string[] Contexts
+    {
+        get
+        {
+            var contextsString = _genesisConfig.GetOrSetValue(ComponentContextsKey, ComponentContextsDefault);
+
+            var contextsArray = contextsString.ArrayFromCSV();
+
+            return contextsArray;
+        }
+        set => _genesisConfig.SetValue(ComponentContextsKey, value.ToCSV());
+    }
+
+
+    public override void Configure(IGenesisConfig genesisConfig)
+    {
+        base.Configure(genesisConfig);
+
+        genesisConfig.SetIfNotPresent(ComponentContextsKey, ComponentContextsDefault);
+    }
+}
