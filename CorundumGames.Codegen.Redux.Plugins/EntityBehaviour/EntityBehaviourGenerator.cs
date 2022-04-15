b87fe89a6f66e564fb83eaafc334bdc7c4de181e
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-04-14 20:21:44 -0400

    feat: Add EntityBehaviour

---
+++ CorundumGames.Codegen.Redux.Plugins/EntityBehaviour/EntityBehaviourGenerator.cs
@@ -0,0 +1,44 @@
+using EntitasRedux.Core.Plugins;
+using Genesis.Plugin;
+using JetBrains.Annotations;
+
+namespace CorundumGames.Codegen.Redux.Plugins.EntityBehaviour;
+
+[PublicAPI]
+public sealed class EntityBehaviourGenerator : AbstractGenerator
+{
+    public override string Name => "Entity Behaviour Generator";
+
+    public override CodeGenFile[] Generate(CodeGeneratorData[] data)
+    {
+        var gameObjectComponentData = data
+            .OfType<ComponentData>()
+            .FirstOrDefault(d => d.ComponentName() == "GameObject");
+
+        if (gameObjectComponentData == null)
+        {
+            return Array.Empty<CodeGenFile>();
+        }
+
+        var contextNames = gameObjectComponentData.GetContextNames();
+
+        return data
+            .OfType<ContextData>()
+            .Where(d => contextNames.Contains(d.GetContextName()))
+            .Select(CreateFile)
+            .ToArray();
+    }
+
+    private static CodeGenFile CreateFile(ContextData context)
+    {
+        var template = new EntityBehaviourTemplate(context);
+
+        var contextName = context.GetContextName();
+
+        return new CodeGenFile(
+            Path.Combine(contextName, $"{contextName}EntityBehaviour.cs"),
+            template.TransformText(),
+            typeof(EntityBehaviourGenerator).FullName
+        );
+    }
+}
