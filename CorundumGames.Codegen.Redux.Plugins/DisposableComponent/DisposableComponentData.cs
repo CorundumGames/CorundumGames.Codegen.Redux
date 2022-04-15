98969cd94da0dcd3dac6fa168df3393b7dc22165
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-04-14 20:17:54 -0400

    fix: Move DisposableComponent to the new namespace

---
+++ CorundumGames.Codegen.Redux.Plugins/DisposableComponent/DisposableComponentData.cs
@@ -0,0 +1,23 @@
+using Genesis.Plugin;
+
+namespace CorundumGames.Codegen.Redux.Plugins.DisposableComponent;
+
+internal sealed class DisposableComponentData : CodeGeneratorData
+{
+    // string keys to access the base dictionary with
+    private const string NameKey = "DisposeData.Name";
+    private const string ContextsKey = "DisposeData.Contexts";
+
+    // wrapper property to access the dictionary
+    public string Name
+    {
+        get => (string)this[NameKey];
+        set => this[NameKey] = value;
+    }
+
+    public string[] Contexts
+    {
+        get => (string[])this[ContextsKey];
+        set => this[ContextsKey] = value;
+    }
+}
