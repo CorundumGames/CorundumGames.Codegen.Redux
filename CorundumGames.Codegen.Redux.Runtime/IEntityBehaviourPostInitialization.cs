50a916bc5debc7ad7252bb0070dae26fdcd3ec82
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-04-14 20:16:07 -0400

    feat: Create CorundumGames.Codegen.Redux.Runtime

---
+++ CorundumGames.Codegen.Redux.Runtime/IEntityBehaviourPostInitialization.cs
@@ -0,0 +1,9 @@
+﻿using JCMG.EntitasRedux;
+
+namespace CorundumGames.Codegen.Redux.Runtime
+{
+    public interface IEntityBehaviourPostInitialization<in TEntity> where TEntity : IEntity
+    {
+        void AfterComponentsAdded(TEntity entity);
+    }
+}
---
+++ CorundumGames.Codegen.Redux.Runtime/IndexByEnumAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CorundumGames.Codegen.Redux.Runtime
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public sealed class IndexByEnumAttribute : Attribute
+    {
+        private readonly Type _componentType;
+
+        public IndexByEnumAttribute(Type componentType)
+        {
+            _componentType = componentType;
+        }
+    }
+}
