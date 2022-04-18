50876d42d5f4f3e53bec705014fb11c4c51a68c9
Jesse Talavera-Greenberg <jesse@jesse.tg>
2022-04-18 18:37:08 -0400

    feat: Remove IEntityBehaviour in favor of the generated blueprint interfaces

--- CorundumGames.Codegen.Redux.Runtime/IEntityBehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using JCMG.EntitasRedux;
-
-namespace CorundumGames.Codegen.Redux.Runtime
-{
-    public interface IEntityBehaviour<in TEntity> where TEntity : IEntity
-    {
-        void AddComponents(TEntity entity);
-    }
-}
--- CorundumGames.Codegen.Redux.Runtime/IEntityBehaviourPostInitialization.cs
+++
@@ -1,9 +0,0 @@
-﻿using JCMG.EntitasRedux;
-
-namespace CorundumGames.Codegen.Redux.Runtime
-{
-    public interface IEntityBehaviourPostInitialization<in TEntity> where TEntity : IEntity
-    {
-        void AfterComponentsAdded(TEntity entity);
-    }
-}
